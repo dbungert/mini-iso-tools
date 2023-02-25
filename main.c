07313a390a56e16de9e4b6a40518a5316ae1a59a
Dan Bungert <daniel.bungert@canonical.com>
2023-02-25 12:38:11 -0700

    experiment

--- main.c
+++ main.c
@@ -39,7 +39,6 @@
 #include "common.h"
 
 #include <locale.h>
-#include <ncurses.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -49,12 +48,11 @@
 #include <stdnoreturn.h>
 #include <sys/param.h>
 
+#include <ncurses.h>
+#include <menu.h>
+
 #include "args.h"
 #include "json.h"
-
-int ubuntu_orange = COLOR_RED;
-int text_white = COLOR_WHITE;
-int back_green = COLOR_GREEN;
 
 noreturn void usage(char *prog)
 {
@@ -69,6 +67,20 @@ typedef enum {
     SELECT=0,
     INCREASE=1,
 } choice_event;
+
+typedef struct _colors {
+    int ubuntu_orange;
+    int text_white;
+    int back_green;
+
+    cchar_t half_block_upper;
+    cchar_t space;
+    cchar_t half_block_lower;
+
+    short black_orange;
+    short white_orange;
+    short white_green;
+} colors_t;
 
 choices_t *read_iso_choices(args_t *args)
 {
@@ -90,7 +102,7 @@ int vertical_center(int len)
     return 3 + (LINES - 3 - len) / 2;
 }
 
-void orange_banner(char *label)
+void orange_banner(colors_t *colors, char *label)
 {
     int x1 = 0;
     int w = COLS;
@@ -98,27 +110,14 @@ void orange_banner(char *label)
     /* Simulate the banner from Subiquity.
      * - draw black on orange for the half-block rows
      * - draw white on orange for the text row */
-    short black_orange = 1;
-    short white_orange = 2;
-    init_pair(black_orange, COLOR_BLACK, ubuntu_orange);
-    init_pair(white_orange, text_white, ubuntu_orange);
 
-    cchar_t half_block_upper;
-    setcchar(&half_block_upper, L"\u2580", 0, black_orange, NULL);
+    mvhline_set(0, x1, &colors->half_block_upper, w);
+    mvhline_set(1, x1, &colors->space, w);
+    mvhline_set(2, x1, &colors->half_block_lower, w);
 
-    cchar_t space;
-    setcchar(&space, L" ", 0, white_orange, NULL);
-
-    cchar_t half_block_lower;
-    setcchar(&half_block_lower, L"\u2584", 0, black_orange, NULL);
-
-    mvhline_set(0, x1, &half_block_upper, w);
-    mvhline_set(1, x1, &space, w);
-    mvhline_set(2, x1, &half_block_lower, w);
-
-    attron(COLOR_PAIR(white_orange));
+    attron(COLOR_PAIR(colors->white_orange));
     mvaddstr(1, horizontal_center(strlen(label)), label);
-    attroff(COLOR_PAIR(white_orange));
+    attroff(COLOR_PAIR(colors->white_orange));
 }
 
 void button(int y, int x, char *label, int textwidth)
@@ -130,11 +129,8 @@ void button(int y, int x, char *label, int textwidth)
     free(button_text);
 }
 
-void add_chooser(choices_t *choices, int selected)
+void add_chooser(colors_t *colors, choices_t *choices)
 {
-    short white_green = 3;
-    init_pair(white_green, text_white, back_green);
-
     int longest = 0;
     for(int i = 0; i < choices->len; i++) {
         longest = MAX(longest, (int)strlen(choices->values[i]->label));
@@ -144,12 +140,12 @@ void add_chooser(choices_t *choices, int selected)
     int center_y = vertical_center(choices->len);
     for(int i = 0; i < choices->len; i++) {
         int y = center_y + i;
-        if(i == selected) {
-            attron(COLOR_PAIR(white_green));
+        if(i == choices->cur) {
+            attron(COLOR_PAIR(colors->white_green));
         }
         button(y, center_x, choices->values[i]->label, longest);
-        if(i == selected) {
-            attroff(COLOR_PAIR(white_green));
+        if(i == choices->cur) {
+            attroff(COLOR_PAIR(colors->white_green));
         }
     }
 }
@@ -215,6 +211,44 @@ void exit_cb(void)
     endwin();
 }
 
+colors_t *setup_colors()
+{
+    if(start_color() == ERR) {
+        syslog(LOG_ERR, "start_color failure");
+        return NULL;
+    }
+
+    colors_t *ret = calloc(sizeof(colors_t), 1);
+    ret->ubuntu_orange = COLOR_RED;
+    ret->text_white = COLOR_WHITE;
+    ret->back_green = COLOR_GREEN;
+
+    syslog(LOG_DEBUG, "can_change_color [%d]", can_change_color());
+    if(can_change_color()) {
+        init_color_from_bytes(ret->ubuntu_orange, 0xE9, 0x54, 0x20);
+        init_color_from_bytes(ret->text_white, 0xFF, 0xFF, 0xFF);
+        init_color_from_bytes(ret->back_green, 0x0E, 0x84, 0x20);
+    } else {
+        /* These are terminal 256 color codes, see
+         * https://www.ditig.com/256-colors-cheat-sheet for an example. */
+        ret->ubuntu_orange = 202;  /* not really but kinda close */
+        ret->text_white = 231;
+        ret->back_green = 28;
+    }
+
+    ret->black_orange = 1;
+    init_pair(ret->black_orange, COLOR_BLACK, ret->ubuntu_orange);
+    ret->white_orange = 2;
+    init_pair(ret->white_orange, ret->text_white, ret->ubuntu_orange);
+    ret->white_green = 3;
+    init_pair(ret->white_green, ret->text_white, ret->back_green);
+
+    setcchar(&ret->half_block_upper, L"\u2580", 0, ret->black_orange, NULL);
+    setcchar(&ret->space, L" ", 0, ret->white_orange, NULL);
+    setcchar(&ret->half_block_lower, L"\u2584", 0, ret->black_orange, NULL);
+    return ret;
+}
+
 int main(int argc, char **argv)
 {
     args_t *args = args_create(argc, argv);
@@ -242,11 +276,6 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    if(start_color() == ERR) {
-        syslog(LOG_ERR, "start_color failure");
-        return 1;
-    }
-
     keypad(stdscr, TRUE);
 
     cbreak();
@@ -254,25 +283,64 @@ int main(int argc, char **argv)
     curs_set(0); /* hide */
     refresh();
 
-    syslog(LOG_DEBUG, "can_change_color [%d]", can_change_color());
-    if(can_change_color()) {
-        init_color_from_bytes(ubuntu_orange, 0xE9, 0x54, 0x20);
-        init_color_from_bytes(text_white, 0xFF, 0xFF, 0xFF);
-        init_color_from_bytes(back_green, 0x0E, 0x84, 0x20);
-    } else {
-        /* These are terminal 256 color codes, see
-         * https://www.ditig.com/256-colors-cheat-sheet for an example. */
-        ubuntu_orange = 202;  /* not really but kinda close */
-        text_white = 231;
-        back_green = 28;
-    }
-
     bool continuing = true;
     int ch = 0;
+    refresh();
+
+    colors_t *colors = setup_colors();
+
+#if 0
+    char *new_choices[3] = {
+        saprintf("[ %s \u25b8 ]", "one"),
+        saprintf("[ %s \u25b8 ]", "two"),
+        saprintf("[ %s \u25b8 ]", "three"),
+    };
+    int n_new_choices = 3;
+
+    ITEM **my_items = (ITEM **)calloc(n_new_choices + 1, sizeof(ITEM *));
+
+    for(int i = 0; i < n_new_choices; ++i)
+        my_items[i] = new_item(new_choices[i], NULL);
+    my_items[n_new_choices] = (ITEM *)NULL;
+
+    WINDOW *my_menu_win = newwin(10, 40, 10, 10);
+    MENU *my_menu = new_menu((ITEM **)my_items);
+    keypad(my_menu_win, TRUE);
+
+    set_menu_win(my_menu, my_menu_win);
+    set_menu_sub(my_menu, derwin(my_menu_win, 6, 38, 1, 1));
+    set_menu_mark(my_menu, "");
+    /* box(my_menu_win, 0, 0); */
+
+    orange_banner("Choose an Ubuntu version to install");
+
+    set_menu_fore(my_menu, COLOR_PAIR(3));
+    post_menu(my_menu);
+    wrefresh(my_menu_win);
+    refresh();
+
+    while((ch = wgetch(my_menu_win)) != KEY_F(1))
+    {       switch(ch)
+        {	case KEY_DOWN:
+            menu_driver(my_menu, REQ_DOWN_ITEM);
+            break;
+            case KEY_UP:
+            menu_driver(my_menu, REQ_UP_ITEM);
+            break;
+            case KEY_NPAGE:
+            menu_driver(my_menu, REQ_SCR_DPAGE);
+            break;
+            case KEY_PPAGE:
+            menu_driver(my_menu, REQ_SCR_UPAGE);
+            break;
+        }
+        wrefresh(my_menu_win);
+    }
+#endif
 
     while(continuing) {
-        orange_banner("Choose an Ubuntu version to install");
-        add_chooser(iso_info, iso_info->cur);
+        orange_banner(colors, "Choose an Ubuntu version to install");
+        add_chooser(colors, iso_info);
         redrawwin(stdscr);
         ch = getch();
         switch(ch) {
