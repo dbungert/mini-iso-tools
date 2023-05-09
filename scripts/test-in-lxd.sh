a796d28ab80ada530d2dbe108341cac6b289afd4
Dan Bungert <daniel.bungert@canonical.com>
2023-05-09 13:23:01 -0600

    experiment

--- scripts/test-in-lxd.sh
+++ scripts/test-in-lxd.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 set -eux
 
-TESTER=$1
+IMAGE=$1
+TESTER=u1
 
+build_deps="build-essential $($(dirname $0)/build-depends.py)"
+
+lxc launch $IMAGE $TESTER
 lxc config device add $TESTER code disk source=`pwd` path=/src
 
 lxc exec $TESTER -- sh -ec "
     cd ~
     cp -a /src .
     [ -d ~/src ]
-    "
-
-lxc exec $TESTER -- cloud-init status --wait
-
-build_deps="build-essential $($(dirname $0)/build-depends.py)"
-
-lxc exec $TESTER -- sh -ec "
     cd ~/src
-    DEBIAN_FRONTEND=noninteractive apt-get update
+    apt-get update
     DEBIAN_FRONTEND=noninteractive apt-get install -y $build_deps
-    dpkg-buildpackage -b -uc"
+    dpkg-buildpackage -b -uc
+"
