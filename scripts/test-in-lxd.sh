87e9f93fafe3ac6174c178310f5691c18ad8fd01
Dan Bungert <daniel.bungert@canonical.com>
2023-05-09 13:39:13 -0600

    experiment

--- scripts/test-in-lxd.sh
+++ scripts/test-in-lxd.sh
@@ -8,11 +8,8 @@ build_deps="build-essential $($(dirname $0)/build-depends.py)"
 
 lxc launch $IMAGE $TESTER
 lxc config device add $TESTER code disk source=`pwd` path=/src
-
 lxc exec $TESTER -- sh -ec "
-    cd ~
-    cp -a /src .
-    [ -d ~/src ]
+    cp -a /src ~
     cd ~/src
     apt-get update
     DEBIAN_FRONTEND=noninteractive apt-get install -y $build_deps
